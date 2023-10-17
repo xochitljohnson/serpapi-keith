086b5ddbfb4d9554b1bbceb50dd7d080106633df
Mac <mac@Macs-Mac-mini.local>
2023-10-16 21:23:46 -0400

    final commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 // basic express server setup
 require('dotenv').config()
-const fs = require('fs')
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
@@ -18,35 +18,44 @@ const params = {
 // use a single (1) GET request to meet requirements
 app.get('/', async (req, res) => {
 
-
-    const getVanGoghPaintingsJson = await getJson(params);
-    const getVanGoghPaintingsHtml = await getHtml(params)
-
-    // render html page in repository
+    try {
+        const getVanGoghPaintingsHtml = await getHtml(params);
+    
+        // render html page in repository
     res.send(getVanGoghPaintingsHtml);
 
-    // push data into array
-   
-   const vanGoghArray = Array.from(getVanGoghPaintingsJson.knowledge_graph.artworks);
+        const getVanGoghPaintingsJson = await getJson(params);
 
+        // push data into array
+        const vanGoghArray = Array.from(getVanGoghPaintingsJson.knowledge_graph.artworks);
+
+        //get result page thumbnail images
+        const thumbnailsArray = getVanGoghPaintingsJson.inline_images;
+
+        // add result page thumbnail images to main array
+        vanGoghArray.push(...thumbnailsArray.map(image => ({
+            title: image.title,
+            thumbnail: image.thumbnail,})))
+
+            const content = JSON.stringify(vanGoghArray);
+            const filePath = 'download.json'
     
-    const thumbnailsArray = getVanGoghPaintingsJson.inline_images;
-    vanGoghArray.push(...thumbnailsArray.map(image => ({
-        title: image.title,
-        thumbnail: image.thumbnail,
-    })))
-
-   //console.log(vanGoghArray)
-
-    // Create a json file with expected array
-    const jsonStr = JSON.stringify(vanGoghArray);
-    const filePath = 'download.json';
-    fs.writeFileSync(filePath, jsonStr);
-    res.download(filePath)
-
-
-    res.end();
+            fs.writeFile(filePath, content, err => {
+                if (!err) {
+                    res.download(filePath)
+                }
+    
+                if (err) {
+                    console.log(err)
+                } return
+            })
+    } catch (error) {
+        res.status(500).send('Internal error. Something went wrong.')
+    }
+    
 })
+
+
 
 // Listen on port 3000
 app.listen(process.env.PORT, console.log('express is running'))
